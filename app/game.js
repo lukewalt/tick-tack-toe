fcc9f916b7c79747ebae10fce06f273d5dcb0a71
Sorrel Brigman <sorrel.k@gmail.com>
2017-02-01 10:37:55 -0600

    fixed turn bug

--- app/game.js
+++ app/game.js
@@ -5,17 +5,18 @@ const xImage = "http://cdn.mysitemyway.com/etc-mysitemyway/icons/legacy-previews
 const oImage ="http://www.drodd.com/images14/o25.png"
 const emptySquare = "http://1.bp.blogspot.com/-jJUO43k6ReU/T7ivfcr4fgI/AAAAAAAAQqU/8YdJwPwT4OE/s1600/transparent.png"
 
+//the game starts with player x
 let currentPlayer = xImage
 
 
 //on click of square,
   //setup click events
   $("td").click(changeSquare) //calls function to change image
-  //listen for changes
 
 
-//changes image to the player's image
-//switches turn to other player
+
+
+
 function changeSquare(evt) {
   //resets the image of the clicked on square to new value
    let clickedSquare = evt.target
@@ -23,17 +24,21 @@ function changeSquare(evt) {
   //if the square is empty
    if(clickedSquare.src === emptySquare) {
     //assign it a new image
+    //changes image to the player's image
     clickedSquare.src = currentPlayer
+    //switches turn to other player
     changePlayer();
    }
-   //change the player
-
-
-}
 //checks for game win
+}
+
 
 //change player
 function changePlayer() {
   //if the current player is x, flip to o, otherwise, flip to x
-  currentPlayer = xImage ? oImage : xImage;
+  if (currentPlayer === xImage) {
+    currentPlayer = oImage;
+  } else {
+    currentPlayer = xImage;
+  }
 }
