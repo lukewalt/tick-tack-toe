1cbb7c6f4ad13e5e928246f2f065a6bde4168322
Sorrel Brigman <sorrel.k@gmail.com>
2017-02-01 10:32:12 -0600

    player changes after successful turn

--- app/game.js
+++ app/game.js
@@ -5,21 +5,35 @@ const xImage = "http://cdn.mysitemyway.com/etc-mysitemyway/icons/legacy-previews
 const oImage ="http://www.drodd.com/images14/o25.png"
 const emptySquare = "http://1.bp.blogspot.com/-jJUO43k6ReU/T7ivfcr4fgI/AAAAAAAAQqU/8YdJwPwT4OE/s1600/transparent.png"
 
+let currentPlayer = xImage
+
+
 //on click of square,
   //setup click events
   $("td").click(changeSquare) //calls function to change image
   //listen for changes
-  //if a square isn't empty, don't allow change
+
 
 //changes image to the player's image
 //switches turn to other player
 function changeSquare(evt) {
   //resets the image of the clicked on square to new value
    let clickedSquare = evt.target
+     //if a square isn't empty, don't allow change
   //if the square is empty
    if(clickedSquare.src === emptySquare) {
     //assign it a new image
-    clickedSquare.src = xImage
+    clickedSquare.src = currentPlayer
+    changePlayer();
    }
+   //change the player
+
+
 }
 //checks for game win
+
+//change player
+function changePlayer() {
+  //if the current player is x, flip to o, otherwise, flip to x
+  currentPlayer = xImage ? oImage : xImage;
+}
