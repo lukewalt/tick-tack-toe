78160c81a994a4df1e18acbeffadbe4f71324ee5
Luke Woodward <luke.wdwrd@gmail.com>
2017-01-31 20:03:31 -0600

    configured firebase

---
+++ app/auth.js
@@ -0,0 +1,7 @@
+firebase.initializeApp({
+  apiKey: "AIzaSyDlmGv53kIXce7x9fsme-GxX7nDQtonuRw",
+  authDomain: "tick-tack-toe-911f1.firebaseapp.com",
+  databaseURL: "https://tick-tack-toe-911f1.firebaseio.com",
+  storageBucket: "tick-tack-toe-911f1.appspot.com",
+  messagingSenderId: "525628931976"
+})
